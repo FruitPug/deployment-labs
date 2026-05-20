760b5f1a56f2c3b7a7e892329b4b64713f94dd9e
FruitPug <roman.pekur@gmail.com>
2026-05-20 19:46:40 +0000

    added deployment verification

--- lab3/scripts/cd/setup-target.sh
+++ lab3/scripts/cd/setup-target.sh
@@ -10,6 +10,7 @@ echo "== Installing packages =="
 curl -fsSL https://get.docker.com | sudo sh
 usermod -aG docker deploy
 apt install docker-compose-plugin -y
+apt install nginx -y
 
 echo "== Creating directory =="
 mkdir -p /opt/mywebapp
---
+++ lab3/scripts/cd/verify.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+BASE_URL="http://localhost"
+
+printf "\nChecking nginx config..."
+sudo nginx -t
+
+printf "\nChecking alive endpoint..."
+curl -f ${BASE_URL}/health/alive
+
+printf "\nChecking ready endpoint..."
+curl -f ${BASE_URL}/health/ready
+
+printf "\nChecking tasks endpoint..."
+curl -f ${BASE_URL}/tasks
+
+printf "\nVerification successful\n"
