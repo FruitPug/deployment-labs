55877548df8caa753ee040ef344a81c2aef8a1db
FruitPug <roman.pekur@gmail.com>
2026-05-20 16:32:46 +0000

    configured target deployment server

---
+++ lab3/scripts/cd/setup-target.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+echo "== Updating system =="
+apt update 
+apt upgrade -y
+
+echo "== Installing packages =="
+curl -fsSL https://get.docker.com | sudo sh
+usermod -aG docker deploy
+apt install docker-compose-plugin -y
+
+echo "== Creating directory =="
+mkdir -p /opt/mywebapp
+chown -R "$USER":"$USER" /opt/mywebapp
+
+echo "== DONE =="
